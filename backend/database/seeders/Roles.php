b6482008d1600a36cde06b947b5cc91f38bbfbda
cristiangomezdev <cristiangomez.dev@gmail.com>
2023-08-18 17:00:48 -0300

    seeders

---
+++ backend/database/seeders/Roles.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Roles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            [
+                'name' => 'admin',
+                'guard_name' => 'web',
+            ],
+            [
+                'name' => 'user',
+                'guard_name' => 'web',
+            ],
+        ];
+
+        foreach ($roles as $role) {
+            \App\Models\Role::create($role);
+        }
+    }
+}
