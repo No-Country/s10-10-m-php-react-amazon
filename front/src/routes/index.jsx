c52e4fd833f0e158ffd2ba5455c6ff6c5937000a
aronresni <aronresni@gmail.com>
2023-08-29 18:54:46 -0300

    shopRegister

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -4,6 +4,8 @@ import Home from "../views/home";
 import LoginUser from "../views/auth/authUser/loginUser";
 import SignUpUser from "../views/auth/authUser/signUpUser";
 import RecoverPass from "../views/auth/authUser/loginUser/recoverPass";
+import SignUpShop from "../views/auth/authShop/signUpShop"
+import LoginShop from "../views/auth/authShop/loginShop";
 const AppRouter = () => {
   return (
     <Router>
@@ -12,6 +14,8 @@ const AppRouter = () => {
       <Route path="/auth/user/signup" component={SignUpUser} />
       <Route path="/auth/user/login" component={LoginUser} />
       <Route path="/recover-pass" component={RecoverPass} />
+      <Route path="/auth/shop/signup" component={SignUpShop} />
+      <Route path="/auth/shop/login" component={LoginShop} />
     </Router>
   );
 };
