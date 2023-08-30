774898d30551eb82bcedff439cf94014c6e4e4b2
WebDevCod <pabloaquino1988@gmail.com>
2023-08-30 18:43:49 -0300

    itemModal added

--- front/src/components/itemDetailContainer/index.jsx
+++ front/src/components/itemDetailContainer/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ItemDetail from "./itemDetail";
-import { params } from 'wouter';
+// import { params } from 'wouter';
 
 const ItemDetailContainer = (props) => {
   const [isLoading, setIsLoading] = useState(true);
