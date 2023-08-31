5b95dd5a82a146e48cd4a559eedf86a7388c91dd
Gustavo Torres <betogus2009@gmail.com>
2023-08-31 02:55:29 -0300

    se realiza la vista dashboard y el cardPack

--- front/src/views/dashboard/index.jsx
+++ front/src/views/dashboard/index.jsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState } from 'react'
+import { InputSearch } from '../../components/InputSearch'
 import ItemListContainer from '../../components/itemListContainer'
+import { products } from '../../utils/products'
+import { faSliders } from '@fortawesome/free-solid-svg-icons'
+import { faMap } from '@fortawesome/free-solid-svg-icons'
 
 const Dashboard = () => {
-    const [products, setProducts] = useState([])
-    /** TO DO consulta al back */
+
+    const [items, setItems] = useState([])
+    
+    useEffect(() => {
+      setItems(products);
+    }, []);
 
   return (
-    <div>
-      <ItemListContainer products={products}/>
+    <div className='p-3'>
+      <div className='p-5 flex items-center justify-center'>
+      <InputSearch/>
+      <FontAwesomeIcon icon={faSliders} className="rotate-90 text-2xl m-2"/>
+      </div>
+      <div className='flex items-center justify-center'>
+      <FontAwesomeIcon icon={faMap} className="text-2xl m-2"/>
+      <span className='underline decoration-1'>CABA, Caballito</span>
+      </div> 
+      <h1 className='text-sizeTitle font-weightTitle'>Los más deseados</h1>
+      <ItemListContainer products={items}/>
+      <h1 className='text-sizeTitle font-weightTitle'>Los más deseados</h1>
+      <ItemListContainer products={items}/>
     </div>
   )
 }
