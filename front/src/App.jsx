417e91a3eb6c3f3f03d0627602f766cc5884c027
WebDevCod <pabloaquino1988@gmail.com>
2023-09-04 20:17:52 -0300

    startPage modified

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,4 @@
+import BasicFooter from './components/BasicFooter'
 import AppRouter from './routes'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <AppRouter />
+      <BasicFooter/>
     </>
   )
 }
--- front/src/views/auth/authUser/signUpUser/startPage/index.jsx
+++ front/src/views/auth/authUser/signUpUser/startPage/index.jsx
@@ -1,48 +1,56 @@
-import { Button, Typography } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
 import { Link } from "wouter";
+import backgroundImg from "./assets/startpage-bg.png";
 
 function SignUpStartPage({ setChoice }) {
   return (
     <>
-      <div className="flex flex-col items-center h-screen bg-mainColor p-[2rem]">
-        <div className="h-[114px] flex justify-center items-center">
-          <h2 className="custom-title p-10 text-center">
-            ¡Bienvenido a ListoParaLlevar!
-          </h2>
-        </div>
-        <p className="text-center custom-subtitle">
-          Regístrate, explora y compra fácilmente alimentos a precios reducidos.
-          ¡Cada producto que salvas marca la diferencia!
-        </p>
-        <section className="mt-64">
-          <div className="flex flex-col space-y-2">
+      <div className="flex flex-col justify-center items-center">
+        <section
+          className="w-full bg-black"
+          style={{
+            backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="text-center text-white m-6">
+            <h1 className="text-2xl font-bold p-8">Te damos la bienvenida a</h1>
+            <h2 className="text-3xl font-normal">LISTO PARA LLEVAR</h2>
+            <p className="italic mt-10">
+              Regístrate, explora y compra fácilmente alimentos a precios
+              reducidos. ¡Cada producto que salvas marca la diferencia!
+            </p>
+            <p className="font-bold m-5">
+              ¡Cada producto que salvas marca la diferencia!
+            </p>
+          </div>
+        </section>
+        <section className="bg-colorPrimary w-full">
+          <div className="flex flex-col items-center space-y-2 mt-10 mb-20">
             <Button
               variant="filled"
-              className="rounded-full normal-case w-72  custom-buttonCTAs "
+              className="rounded-full normal-case w-72 bg-buttonFilledColor text-colorPrimary text-sm custom-google-button"
             >
-              Registrate con Google
+              Registrarme con Google
             </Button>
             <Button
               variant="filled"
-              className="rounded-full normal-case w-72 custom-buttonCTAs"
+              className="rounded-full normal-case w-72 bg-buttonFilledColor text-colorPrimary text-sm custom-facebook-button"
             >
-              Registrate con Facebook
+              Registrarme con Facebook
             </Button>
             <Button
               variant="filled"
-              className="rounded-full normal-case w-72 custom-buttonCTAs"
+              className="rounded-full normal-case w-72 bg-buttonFilledColor text-colorPrimary text-sm custom-mail-button"
               onClick={() => setChoice("email")}
             >
-              Registrate con mail
+              Registrarme con correo
             </Button>
-            <Typography
-              variant="small"
-              className="mt-6 flex justify-between custom-textButton pb-8"
-            >
-              <span>¿Ya tienes cuenta?</span>
-
-              <Link to="/auth/user/login" >Inicia sesión acá</Link>
-            </Typography>
+            <div className="flex text-white text-sm">
+              <p className="mr-2">¿Ya tienes una cuenta?</p>
+              <Link to="/auth/user/login">Inicia sesión acá</Link>
+            </div>
           </div>
         </section>
       </div>
