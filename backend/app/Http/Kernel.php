96cec4ea31adb40f624940af3337514c0ad046b0
OsdaGomez99 <ogomez239@e.uneg.edu.ve>
2023-09-07 15:15:32 -0500

    Arreglo de CORS

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -37,15 +37,17 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-             \App\Http\Middleware\Cors::class,
- 
+            \App\Http\Middleware\Cors::class,
+
+
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-           
+            \App\Http\Middleware\Cors::class,
+
         ],
     ];
 
@@ -69,6 +71,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         //  'cors' => \App\Http\Middleware\Cors::class,
-        
+
     ];
 }
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://s10-10-m-php-react.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
