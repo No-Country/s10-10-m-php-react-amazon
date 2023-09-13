b1132bbd5e198516fdd97156640d3c23734f63d3
WebDevCod <pabloaquino1988@gmail.com>
2023-09-13 18:02:58 -0300

    home explorecategories modified in classname to add w-10/12

--- front/src/views/home/components/exploreCategories/index.jsx
+++ front/src/views/home/components/exploreCategories/index.jsx
@@ -1,13 +1,11 @@
+import Cards from "./cards";
 
-import Cards from './cards';
-
-import panaderia from '../../assets/panaderia1.jpg';
-import verduleria from '../../assets/verduleria1.jpg';
-import superMercado from '../../assets/superMercado1.jpg';
-import Slider from 'react-slick';
+import panaderia from "../../assets/panaderia1.jpg";
+import verduleria from "../../assets/verduleria1.jpg";
+import superMercado from "../../assets/superMercado1.jpg";
+import Slider from "react-slick";
 
 const ExploreCategories = () => {
-
   var settings = {
     dots: false,
     infinite: false,
@@ -20,45 +18,70 @@ const ExploreCategories = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-        }
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-        }
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-        }
-      }
-    ]
+        },
+      },
+    ],
   };
 
   return (
-    <div className='pl-[2rem] lg:px-[2rem] mb-12 w-full lg:mb-24'>
-      <h2 className='font-bold text-colorNeutral1 text-left leading-none text-sizeSubtitle my-10'>
-        Explora nuestros rubros
-      </h2>
+    <>
+      <div className="w-10/12 mb-12">
+        <h2 className="font-bold text-colorNeutral1 text-left leading-none text-sizeSubtitle my-10">
+          Explora nuestros rubros
+        </h2>
 
-      <div className='lg:hidden'>
-        <Slider {...settings} className=''>
-          <Cards img={panaderia} title={"Panaderías"} category={"panaderia"} />
-          <Cards img={verduleria} title={"Verdulerías"} category={"verduleria"} />
-          <Cards img={superMercado} title={"Supermercados"} category={"supermercado"} />
-        </Slider>
-      </div>
-      <div className='hidden lg:block'>
-        <div className='flex justify-between gap-6'>
-          <Cards img={panaderia} title={"Panaderías"} category={"panaderia"} />
-          <Cards img={verduleria} title={"Verdulerías"} category={"verduleria"} />
-          <Cards img={superMercado} title={"Supermercados"} category={"supermercado"} />
+        <div className="lg:hidden">
+          <Slider {...settings} className="">
+            <Cards
+              img={panaderia}
+              title={"Panaderías"}
+              category={"panaderia"}
+            />
+            <Cards
+              img={verduleria}
+              title={"Verdulerías"}
+              category={"verduleria"}
+            />
+            <Cards
+              img={superMercado}
+              title={"Supermercados"}
+              category={"supermercado"}
+            />
+          </Slider>
+        </div>
+        <div className="hidden lg:block">
+          <div className="flex justify-between gap-2">
+            <Cards
+              img={panaderia}
+              title={"Panaderías"}
+              category={"panaderia"}
+            />
+            <Cards
+              img={verduleria}
+              title={"Verdulerías"}
+              category={"verduleria"}
+            />
+            <Cards
+              img={superMercado}
+              title={"Supermercados"}
+              category={"supermercado"}
+            />
+          </div>
         </div>
       </div>
-
-    </div >
+    </>
   );
 };
 
