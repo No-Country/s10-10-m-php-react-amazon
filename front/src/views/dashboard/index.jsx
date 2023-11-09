56807928379a6782a85e37d23d9e8eb7c80c897a
Gustavo Torres <betogus2009@gmail.com>
2023-11-09 12:00:34 -0300

    se corrige el dashboard (conflictos)

--- front/src/views/dashboard/index.jsx
+++ front/src/views/dashboard/index.jsx
@@ -39,16 +39,8 @@ const Dashboard = () => {
   const handleFilter = () => {
     setShowFilter(!showFilter);
   };
-<<<<<<< HEAD
-<<<<<<< HEAD
+  
   const skeleton = [1, 2, 3, 4, 5, 6];
-=======
-  const skeleton = [1, 2, 3, 4, 5, 6]
->>>>>>> front-dev
-=======
-
-  const skeleton = [1, 2, 3, 4, 5, 6]
->>>>>>> front-dev
   return (
     <div
       className={
